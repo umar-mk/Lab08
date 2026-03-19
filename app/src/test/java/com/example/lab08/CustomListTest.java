e860c39033d9517168aec81615c9b1ad559fcb7c
umar-mk <mumarmalik935@gmail.com>
2026-03-19 19:40:46 +0500

    test failing check

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,14 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
 
+        list.addCity(calgary);
+
+        // This should fail at first
+        assertTrue(list.hasCity(calgary));
+    }
 }
